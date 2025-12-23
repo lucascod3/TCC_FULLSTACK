303a83aba58c7def6fbbc465d50660ebeb6e6d8c
lucascod3 <lukinha_matheus09@hotmail.com>
2025-12-23 03:35:17 -0300

    Quarto commit

---
+++ BibliotecaWeb/Models/Livros.cs
@@ -0,0 +1,12 @@
+namespace BibliotecaWeb.Models
+{
+    public class Livro
+    {
+        public int Numero { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Ano { get; set; }
+        public string Descricao { get; set; }
+        public string CapaUrl { get; set; }
+    }
+}
---
+++ BibliotecaWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BibliotecaWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BibliotecaWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Livro> Livros { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Busca { get; set; }
+
+        public void OnGet()
+        {
+            var lista = new List<Livro>
+            {
+                new Livro { Numero = 1, Titulo = "Algoritmos e Estruturas de Dados", Autor = "N. Wirth", Ano = 1976, Descricao = "Fundamentos da programação.", CapaUrl = "/images/algoritmos_e_estruturas_de_dados.jpg" },
+                new Livro {
+    Numero = 2,
+    Titulo = "Clean Code",
+    Autor = "Robert C. Martin",
+    Ano = 2008,
+    Descricao = "Código limpo e sustentável.",
+    CapaUrl = "/images/clean-code.jpg"
+},
+                new Livro { Numero = 3, Titulo = "Design Patterns", Autor = "Erich Gamma et al.", Ano = 1994, Descricao = "Padrões de projeto.", CapaUrl = "/images/design_patterns.jpg" },
+                new Livro { Numero = 4, Titulo = "Inteligência Artificial: Uma Abordagem Moderna", Autor = "Russell & Norvig", Ano = 2010, Descricao = "Livro referência em IA.", CapaUrl = "/images/Inteligência_Artificial_Uma_Abordagem_Moderna.jpg" },
+                new Livro { Numero = 5, Titulo = "Microsoft Visual C# Step by Step (Eighth Edition)", Autor = "John Sharp", Ano = 2015, Descricao = "Guia prático para aprender C#.", CapaUrl = "images/Microsoft_Visual.jpg"},
+            };
+
+            // Se o usuário digitou algo, filtra
+            if (!string.IsNullOrEmpty(Busca))
+            {
+                Livros = lista
+                    .Where(l =>
+                        l.Titulo.Contains(Busca, System.StringComparison.OrdinalIgnoreCase) ||
+                        l.Numero.ToString() == Busca)
+                    .OrderBy(l => l.Titulo)
+                    .ToList();
+            }
+            else
+            {
+                Livros = lista.OrderBy(l => l.Titulo).ToList();
+            }
+        }
+    }
+}
